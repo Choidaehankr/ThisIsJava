fe945fd656bcc54548ad1ba22aeb943cfcaaa013
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-03-28 23:15:36 +0900

    Class -review

---
+++ src/classExample/Student.java
@@ -0,0 +1,11 @@
+package classExample;
+
+public class Student {
+    // 아래와 같이 main() 메소드를 작성해서 라이브러리인 동시에 실행클래스로 만들 수 있음.
+//    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        System.out.println("s1 변수가 Student 객체를 참조");
+//        Student s2 = new Student();
+//        System.out.println("s2 변수가 또 다른 Student 객체를 참조");
+//    }
+}
